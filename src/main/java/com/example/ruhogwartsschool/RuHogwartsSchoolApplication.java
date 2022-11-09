d615c8a496be785d2498820648674e8a74cb3f2b
Anastasiyu <asy.safiulina@mail.ru>
2022-11-09 18:19:27 +0300

    ДЗ 3.5

---
+++ src/main/java/com/example/ruhogwartsschool/RuHogwartsSchoolApplication.java
@@ -0,0 +1,14 @@
+package com.example.ruhogwartsschool;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@OpenAPIDefinition
+@SpringBootApplication
+public class RuHogwartsScoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuHogwartsScoolApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/ruhogwartsschool/entity/Faculty.java
@@ -0,0 +1,83 @@
+package com.example.ruhogwartsschool.entity;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Faculty {
+    @javax.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String color;
+
+
+    @OneToMany(mappedBy = "faculty")
+    private Collection<Student> students;
+
+
+
+    public Collection<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Collection<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Факультет №" + id + ":" + name + ", " + color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Faculty faculty = (Faculty) o;
+        return id == faculty.id && Objects.equals(name, faculty.name) && Objects.equals(color, faculty.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, color);
+    }
+
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
