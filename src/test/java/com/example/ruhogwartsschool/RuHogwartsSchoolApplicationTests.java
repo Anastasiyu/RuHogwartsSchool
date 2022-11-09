fee3d35bf62a06afaffb4ddc9dfe7275152cb4be
Anastasiyu <asy.safiulina@mail.ru>
2022-11-09 18:14:27 +0300

    Initial commit

---
+++ src/test/java/com/example/ruhogwartsschool/RuHogwartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruhogwartsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuHogwartsSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
